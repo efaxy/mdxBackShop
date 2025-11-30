d66e5ab475df3956e3b21846d6606c5eaecd6bd2
yuma <nak071104@gmail.com>
2025-11-30 22:46:46 +0400

    remove test user creation from DB connection

--- index.js
+++ index.js
@@ -7,11 +7,6 @@ const start = async () => {
 	try {
 		await client.connect()
 		console.log('Connected to MongoDB')
-		await client.db().collection('userrrrr')
-		const users = client.db().collection('userrrrr')
-		await users.insertOne({ name: 'Efaxy', age: 19 })
-		const user = await users.findOne({ name: 'Efaxy' })
-		console.log(user)
 	} catch (err) {
 		console.log(err)
 	}
