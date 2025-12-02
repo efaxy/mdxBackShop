0dec8efdb38a16a92cf8e64f8dbb0e0865d452f4
efaxy <nak071104@gmail.com>
2025-12-02 12:54:46 +0400

    add CORS middleware for handling cross-origin requests

--- index.js
+++ index.js
@@ -24,4 +24,21 @@ MongoClient.connect(url, (error, client) => {
 
 // app.use("/image", (req, res) => {	});
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.listen(3000);
